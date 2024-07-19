d300e02eaeb1c417f6f6ac3f01a2a17618c956c7
abhay9656 <abhay9656gupta@gmail.com>
2024-07-19 13:27:00 +0530

    Update background color in App.jsx and add random number generation in guess.jsx

--- component/guess.jsx
+++ component/guess.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 const guess = () => {
+
+    const [number, setNumber] = useState(0)
+
+    const randomNumber = () => {
+        let num = Math.floor(Math.random() * 100) + 1
+        setNumber(num)
+    }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Guess NUmber</Text>
-
+      <Text style={styles.text}>Guess Number</Text>
+        <Text style={styles.num}>{number}</Text>
+        <Button title='Generate a numnber' onPress={randomNumber}/>
     </View>
   )
 }
@@ -14,9 +23,20 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 30,
         color: 'blue',
+        fontWeight: 'bold',
+        marginBottom: 20
     },
     container:{
-        marginTop:50
+        marginTop:50,
+        
+        
+    },
+    num:{
+        fontSize: 50,
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center'
+
     }
 })
 
